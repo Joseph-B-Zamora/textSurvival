18ccc3ed11e156ad3b1c57d645a6d68ae4a41059
Joseph-B-Zamora <115887027+Joseph-B-Zamora@users.noreply.github.com>
2023-01-24 11:06:56 -0700

    Add files via upload

---
+++ js/indexScript.js
@@ -0,0 +1,15 @@
+let curHe = 100;
+let maxHe = 100;
+let heVal = curHe+"/"+maxHe;
+
+let curSt = 100;
+let maxSt = 100;
+let stVal = curSt+"/"+maxSt;
+
+let curHu = 0;
+let maxHu = 100;
+let huVal = curHu+"/"+maxHu;
+
+let curTh = 0;
+let maxTh = 100;
+let thVal = curTh+"/"+maxTh;
